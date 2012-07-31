aea44b97d29e46c99aba49702fc867f4a2239010
Sérgio Lopes <sergio.lopes@caelum.com.br>
2012-07-31 19:00:18 -0300

    script run: roda jekyll só pra gerar e pyhton SimpleHTTPServer pra servir local

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-jekyll --server --auto --limit_posts 1
+# kill jekyll after this script is killed
+trap "killall ruby" EXIT
+
+# starts jekyll in no-server mode
+jekyll --auto --limit_posts 1 &
+
+# starts python server
+cd _site
+python -m SimpleHTTPServer 4000
