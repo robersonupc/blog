e6764854088259fae8f5ad9f8f753829fd3399fb
Sérgio Lopes <sergio.lopes@caelum.com.br>
2012-08-01 00:49:44 -0300

    code highlight agora suporta identacao do bloco todo

--- _plugins/code_tag.rb
+++ _plugins/code_tag.rb
@@ -3,7 +3,30 @@ module Jekyll
 
     def initialize(tag_name, text, tokens)
       super(tag_name, text + ' linenos', tokens)
+    end
+
+    def render_pygments(context, code)
       
+      newcode = ''
+      
+      first_line_analyzed = false
+      initial_spaces = ''
+
+      code.each_line do |line|
+      	if not first_line_analyzed
+      		next if line.strip == ''
+
+      		first_line_analyzed = true
+
+			if line =~ /^(\s+)/
+				initial_spaces = $1
+			end
+      	end
+
+      	newcode << line.gsub(Regexp.new('^' + initial_spaces), '')
+      end
+
+      super(context, newcode)
     end
   end
 end
