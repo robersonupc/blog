cd76aa38c23488de9f74ff582b9ef11056a18d9a
Sérgio Lopes <sergio.lopes@caelum.com.br>
2013-01-12 19:20:07 -0200

    relayout: saem widgets sociais

--- js/scripts.js
+++ js/scripts.js
@@ -26,25 +26,6 @@ _gaq.push(['_trackPageview']);
 	}
 })();
 
-// loads facebook
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=309652569089723";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-// load twitter
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
-
-// load google+
-(function() {
-    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-    po.src = 'https://apis.google.com/js/plusone.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-})();
-
 // analytics: track external links
 (function(doc) {
 	if (!document.querySelectorAll) return;
